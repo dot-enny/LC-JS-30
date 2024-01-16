1718bc889355e63652a76c52f520065399dd4982
OlaniyanEniola <olaniyaneniola0225@gmail.com>
2024-01-16 21:13:07 +0100

    snippet: Chunk Array.

---
+++ ChunkArray.js
@@ -0,0 +1,12 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+const chunk = function(arr, size) {
+    const chunkedArr = [];
+    for (let i = 0; i < arr.length; i+=size) {
+        chunkedArr.push(arr.slice(i, i+size))
+    };
+    return chunkedArr;
+};
