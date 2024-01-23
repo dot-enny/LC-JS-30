6ab46f7e777d21eb5d7740b7aab556d19551db92
OlaniyanEniola <olaniyaneniola0225@gmail.com>
2024-01-23 21:56:28 +0100

    update: Flatten Deeply Nested Arrays.

--- FlattenDeeplyNestedArr.js
+++ FlattenDeeplyNestedArr.js
@@ -3,10 +3,25 @@
  * @param {number} depth
  * @return {Array}
  */
+const matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+const flattenedMatrix = matrix.flat();
+// Result: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+const nestedArray = [1, [2, [3, [4, 5]]]];
+const flattenedArray = nestedArray.flat(2);
+// Result: [1, 2, 3, 4, 5]
+
+// THE FLAT METHOD TAKES AN ARGUMENT WHICH SPECIFIES THE 'DEPTH'
+// TO WHICH THE ARR SHOULD BE FLATTENED
+
+const nestedData = { user: { details: { name: 'John', age: 25 } } };
+const flattenedData = Object.values(nestedData).flat();
+// Result: ['John', 25]
+// HOW IT WORKS UNDER THE HOOD
+
 const flat = function (arr, n) {
     if (n == 0) return arr;
     const flattenedArr = [];
-
     const flattenArr = (innerArr, depth) => {
         for (const item of innerArr) {
             if (typeof item === 'object' && depth < n) {
@@ -17,7 +32,6 @@ const flat = function (arr, n) {
         };
         return flattenedArr;
     };
-
     return flattenArr(arr, 0);
 };
 const arr = [1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]];
