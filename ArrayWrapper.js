c5273c9eb3903ef4a4f72de4350f7d22229f0aa8
OlaniyanEniola <olaniyaneniola0225@gmail.com>
2024-02-01 21:44:20 +0100

    snippet: Array Wrapper.

---
+++ ArrayWrapper.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {void}
+ */
+class ArrayWrapper {
+    constructor(nums) {
+        this.nums = nums;
+    };
+    valueOf() {
+        return this.nums.reduce((num, acc) => num + acc, 0);
+    };
+    toString() {
+        return `[${String(this.nums)}]`;
+    };
+};
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
